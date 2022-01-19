95346b76d353654c9cb6aa2a4406f31f65db78b4
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-19 16:45:14 +0200

    Create ServerSocketBase.cs

---
+++ PingPong.Server/Sockets/Abstractions/ServerSocketBase.cs
@@ -0,0 +1,22 @@
+﻿using PingPong.Server.Listeners.Abstractions;
+using System.Threading.Tasks;
+
+namespace PingPong.Server.Sockets.Abstractions
+{
+    public abstract class ServerSocketBase : IBinder, IListener
+    {
+        protected readonly string _ipAddress;
+        protected readonly uint _port;
+
+        protected ServerSocketBase(string ipAddress, uint port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public abstract Task Accept();
+        public abstract Task Bind();
+        public abstract Task Listen();
+        public abstract Task Close();
+    }
+}
