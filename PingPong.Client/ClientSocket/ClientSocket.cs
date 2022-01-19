bdae61a5f41081067635378f96fc6ba4110efa72
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-19 17:51:01 +0200

    added implementation to client socket

---
+++ PingPong.Client/ClientSocket/ClientSocket.cs
@@ -0,0 +1,38 @@
+﻿using PingPong.Common.Sockets.Client.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Client.ClientSocket
+{
+    public class ClientSocket : ClientSocketBase<string>
+    {
+        private Socket _socket;
+
+        public ClientSocket(Socket socket, string ipAddress, uint port):
+            base(ipAddress, port)
+        {
+            _socket = socket;
+        }
+
+        public override async Task Connect()
+        {
+            await _socket.ConnectAsync(_ipAddress, (int)_port);
+        }
+
+        public override string GetInputObject()
+        {
+            var message = new List<Byte>();
+            _socket.Receive((IList<ArraySegment<byte>>)message);
+            return Encoding.ASCII.GetString(message.ToArray());
+        }
+
+        public override void SendOutput(string outputObject)
+        {
+            var message = Encoding.ASCII.GetBytes(outputObject);
+            _socket.Send(message);
+        }
+    }
+}
