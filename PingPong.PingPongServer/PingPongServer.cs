dadaa9f020983bb754e5938d1a4e278e1c39411f
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 11:07:04 +0200

    added implementation to server

---
+++ PingPong.PingPongServer/PingPongServer.cs
@@ -0,0 +1,40 @@
+﻿using PingPong.Common.Sockets.Client.Abstractions;
+using PingPong.Server.Sockets.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PingPong.PingPongServer
+{
+    public class PingPongServer
+    {
+        private ServerSocketBase _serverListener;
+
+        public PingPongServer(ServerSocketBase serverSocket)
+        {
+            _serverListener = serverSocket;
+        }
+
+        public async Task SetupServer(int numberOfActiveConnections, CancellationToken token)
+        {
+            await _serverListener.Bind();
+            await _serverListener.Listen(numberOfActiveConnections);
+
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+
+                var newClient = await _serverListener.Accept();
+
+            }
+        }
+
+        private async Task HandleClient(ClientSocketBase<string> newClient)
+        {
+            
+        }
+    }
+}
