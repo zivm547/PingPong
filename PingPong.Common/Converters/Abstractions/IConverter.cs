f29cf1a15f69a7d05deb8196ce75debef9ab16b3
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 11:41:55 +0200

    Create IConverter.cs

---
+++ PingPong.Common/Converters/Abstractions/IConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace PingPong.Common.Converters.Abstractions
+{
+    public interface IConverter<TSourceObject, TOutputObject>
+    {
+        Task<bool> TryConvert(TSourceObject sourceObject, TOutputObject outputObject);
+    }
+}
