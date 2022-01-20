4d2491fd2f2639046852a28c23fa8bd4d85cb4d6
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 11:34:22 +0200

    Create ListenerSocketBase.cs

---
+++ PingPong.Networking/Wrappers/Sockets/ListenerSockets/Abstractions/ListenerSocketBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PingPong.Networking.Wrappers.Sockets.ListenerSockets.Abstractions
+{
+    public abstract class ListenerSocketBase
+    {
+        public abstract Task Bind(EndPoint endPoint);
+        public abstract Task Listen();
+        public abstract Task<Socket> Accept();
+        public abstract Task Close();
+    }
+}
