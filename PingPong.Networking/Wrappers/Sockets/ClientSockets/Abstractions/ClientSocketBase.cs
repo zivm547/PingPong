2333f7134143d63722f83886ceebeb430f4fe174
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 11:23:13 +0200

    added client socket base

---
+++ PingPong.Networking/Wrappers/Sockets/ClientSockets/Abstractions/ClientSocketBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace PingPong.Networking.Wrappers.Sockets.ClientSockets.Abstractions
+{
+    public abstract class ClientSocketBase
+    {
+        public abstract Task Connect(IPEndPoint ip, int port);
+        public abstract Task Send(byte[] buffer);
+        public abstract Task<byte[]> Receive();
+        public abstract Task Close();
+    }
+}
