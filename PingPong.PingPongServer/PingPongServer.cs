74ac8d8a6a929212227f7cd4b7fe5ab175200b4c
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 14:51:28 +0200

    added implementation to server

--- PingPong.PingPongServer/PingPongServer.cs
+++ PingPong.PingPongServer/PingPongServer.cs
@@ -1,6 +1,8 @@
-﻿using PingPong.Factories.Abstractions;
+using PingPong.Common.Converters.Abstractions;
+using PingPong.Factories.Abstractions;
 using PingPong.Networking.Wrappers.Sockets.ClientSockets.Abstractions;
 using PingPong.Networking.Wrappers.Sockets.ListenerSockets.Abstractions;
+using System;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Threading;
@@ -14,13 +16,21 @@ namespace PingPong.PingPongServer
         private EndPoint _endPoint;
         private ConcurrentBag<Task> _activeClients;
         private IClientSocketFactory _clientSocketFactory;
+        private IConverter<string, byte[]> _stringToByteConverter;
+        private IConverter<byte[], string> _byteToStringConverter;
 
-        public PingPongServer(ListenerSocketBase serverListener, EndPoint endPoint, IClientSocketFactory clientSocketFactory)
+        public PingPongServer(ListenerSocketBase serverListener, 
+            EndPoint endPoint, 
+            IClientSocketFactory clientSocketFactory, 
+            IConverter<string, byte[]> stringToByteConverter, 
+            IConverter<byte[], string> byteToStringConverter)
         {
             _serverListener = serverListener;
             _endPoint = endPoint;
             _activeClients = new ConcurrentBag<Task>();
             _clientSocketFactory = clientSocketFactory;
+            _stringToByteConverter = stringToByteConverter;
+            _byteToStringConverter = byteToStringConverter;
         }
 
         public async Task StartServer(CancellationToken token)
@@ -43,7 +53,22 @@ namespace PingPong.PingPongServer
 
         private async Task HandleClient(ClientSocketBase newClient)
         {
-            
+            while (true)
+            {
+                try
+                {
+                    string message;
+                    var clientMessage = await newClient.Receive();
+                    _byteToStringConverter.TryConvert(clientMessage, out message);
+
+                    await newClient.Send(clientMessage);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
         }
     }
 }
