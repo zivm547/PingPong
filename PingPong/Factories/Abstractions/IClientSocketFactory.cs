734594f1c2e17c58869c5c8ec43ab9d50dd735f2
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 12:54:38 +0200

    Create IClientSocketFactory.cs

---
+++ PingPong/Factories/Abstractions/IClientSocketFactory.cs
@@ -0,0 +1,10 @@
+﻿using PingPong.Networking.Wrappers.Sockets.ClientSockets.Abstractions;
+using System.Net.Sockets;
+
+namespace PingPong.Factories.Abstractions
+{
+    public interface IClientSocketFactory
+    {
+        ClientSocketBase CreateNewClientSocket(Socket socket);
+    }
+}
