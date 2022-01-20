e14dedcb4950b2425120067a31803428f0d09fb1
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 10:46:00 +0200

    added implementation of pingpong client

--- PingPong.PingPongClient/PingPongClient.cs
+++ PingPong.PingPongClient/PingPongClient.cs
@@ -1,9 +1,11 @@
 ﻿using PingPong.Common.Sockets.Client.Abstractions;
 using PingPong.IO.Inputs;
+using PingPong.IO.Outputs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PingPong.PingPongClient
@@ -12,11 +14,13 @@ namespace PingPong.PingPongClient
     {
         private ClientSocketBase<string> _socket;
         private IReader<string> _inputSource;
+        private IWriter<string> _output;
 
-        public PingPongClient(ClientSocketBase<string> socket, IReader<string> inputSource)
+        public PingPongClient(ClientSocketBase<string> socket, IReader<string> inputSource, IWriter<string> output)
         {
             _socket = socket;
             _inputSource = inputSource;
+            _output = output;
         }
 
         public async Task StartConnection()
@@ -24,6 +28,39 @@ namespace PingPong.PingPongClient
             await _socket.Connect();
         }
 
+        public async Task StartPingPong(CancellationToken token)
+        {
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
 
+                var messageToSendToServer = await GetInput();
+                await SendServerMessage(messageToSendToServer);
+
+                var receiveMessageFromServer = ReceiveMessageFromServer();
+                var serverResponse = await receiveMessageFromServer;
+
+                await _output.Write(serverResponse);
+            }
+        }
+
+        private async Task SendServerMessage(string message)
+        {
+            var writingMessageToServer = _socket.Write(message);
+            await writingMessageToServer;
+        }
+
+        private async Task<string> GetInput()
+        {
+            var gettingInput = _inputSource.Read();
+            var userInput = await gettingInput;
+            return userInput;
+        }
+
+        private async Task<string> ReceiveMessageFromServer()
+        {
+            var receivingMessage = _socket.Read();
+            return await receivingMessage;
+        }
     }
 }
