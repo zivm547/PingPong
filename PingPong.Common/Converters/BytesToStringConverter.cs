36d679e82806f445bb1f340de0ed1950f2a8fcb9
zivm547 <69574069+zivm547@users.noreply.github.com>
2022-01-20 12:18:07 +0200

    added byte to string converter

---
+++ PingPong.Common/Converters/BytesToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using PingPong.Common.Converters.Abstractions;
+using System;
+using System.Text;
+
+namespace PingPong.Common.Converters
+{
+    public class BytesToStringConverter : IConverter<byte[], string>
+    {
+        public bool TryConvert(byte[] sourceObject, out string outputObject)
+        {
+            try
+            {
+                outputObject = Encoding.ASCII.GetString(sourceObject);
+            }
+            catch (Exception)
+            {
+                outputObject = null;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- PingPong.Networking/Wrappers/Sockets/ClientSockets/ClientSocket.cs
+++ PingPong.Networking/Wrappers/Sockets/ClientSockets/ClientSocket.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PingPong.Networking.Wrappers.Sockets.ClientSockets
